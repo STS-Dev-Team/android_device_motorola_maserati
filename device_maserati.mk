fbdad0cdbf2275f4233cbf4cb36e591a12c932e5
David Hacker <davidhackerdvm@gmail.com>
2012-08-22 01:32:53 -0500

    update wifi tether

--- device_maserati.mk
+++ device_maserati.mk
@@ -98,7 +98,7 @@ PRODUCT_PACKAGES += \
     Apollo \
 
 # WirelessTether
-PRODUCT_PACKAGES += wifi_tether_v3_2-pre1
+PRODUCT_PACKAGES += wifi_tether_v3_2-beta1
 PRODUCT_COPY_FILES += \
     device/motorola/maserati/prebuilt/lib/libwtnativetask.so:system/lib/libwtnativetask.so \
 
