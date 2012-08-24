3d46c0184e1981e435ece5dc222da079dce2cc36
Hashcode <hashcode0f@gmail.com>
2012-08-24 00:45:52 -0700

    add build optimizations back in
    Change-Id: Ic88c3abcaa6c3e2599c00a64b80e3ab6cedb8edf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,6 +155,18 @@ TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/motorola/maserati/releasetoo
 TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/motorola/maserati/releasetools/maserati_img_from_target_files
 TARGET_CUSTOM_RELEASETOOL := ./device/motorola/maserati/releasetools/squisher
 
+# CodeAurora Optimizations: msm8960: Improve performance of memmove, bcopy, and memmove_words
+# added by twa_priv
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_KRAIT_BIONIC_PLDOFFS := 10
+TARGET_KRAIT_BIONIC_PLDTHRESH := 10
+TARGET_KRAIT_BIONIC_BBTHRESH := 64
+TARGET_KRAIT_BIONIC_PLDSIZE := 64
+
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+
 
 # Misc.
 BOARD_USE_BATTERY_CHARGE_COUNTER := true
