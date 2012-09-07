d53e3eedfa06f055c71f5687173c314e691c0328
Hashcode <hashcode0f@gmail.com>
2012-09-07 11:25:51 -0700

    adjust for common config
    Change-Id: I4449510c8af42b2d36ca99ce8cba0dc95808429b

--- device.mk
+++ device.mk
@@ -4,21 +4,8 @@
 
 DEVICE_FOLDER := device/motorola/maserati
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
-
 # Device overlay
-    DEVICE_PACKAGE_OVERLAYS += $(DEVICE_FOLDER)/overlay
-
-# high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
-PRODUCT_PACKAGES := \
-    charger \
-    charger_res_images
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_FOLDER)/overlay
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -29,71 +16,11 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/audio/libasound.so:/system/lib/libasound.so \
     $(DEVICE_FOLDER)/audio/libaudio_ext.so:/system/lib/libaudio_ext.so
 
-# Hardware HALs
-PRODUCT_PACKAGES += \
-    camera.omap4 \
-    libinvensense_mpl
-
-PRODUCT_PACKAGES += \
-    libaudioutils \
-    libaudiohw_legacy
-
-# BlueZ test tools
-PRODUCT_PACKAGES += \
-    hciconfig \
-    hcitool
-
 # Modem
 PRODUCT_PACKAGES += \
     nc \
     Stk \
-    libaudiomodemgeneric \
-    libreference-cdma-sms \
-    rild \
-    radiooptions
-
-# Wifi
-PRODUCT_PACKAGES += \
-    lib_driver_cmd_wl12xx \
-    dhcpcd.conf \
-    hostapd.conf \
-    wifical.sh \
-    wpa_supplicant.conf \
-    TQS_D_1.7.ini \
-    crda \
-    regulatory.bin \
-    calibrator
-
-# Wifi Direct
-PRODUCT_PACKAGES += \
-    ti_wfd_libs
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    bt_sco_app \
-    uim-sysfs 
-
-# Release utilities
-PRODUCT_PACKAGES += \
-    maserati_releaseutils-check_kernel \
-    maserati_releaseutils-finalize_release \
-    maserati_releaseutils-mke2fs \
-    maserati_releaseutils-tune2fs
-
-PRODUCT_PACKAGES += \
-    evtest \
-    sensor.test \
-    camera_test \
-    Superuser \
-    su \
-    DockAudio \
-    safestrapmenu
-
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory \
-    FileManager \
-    MusicFX \
-    Apollo
+    libreference-cdma-sms
 
 # Rootfs files
 PRODUCT_COPY_FILES += \
@@ -107,42 +34,16 @@ PRODUCT_COPY_FILES += \
 
 # Kexec files
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/kexec/arm_kexec.ko:system/etc/kexec/arm_kexec.ko \
-    $(DEVICE_FOLDER)/kexec/atags:system/etc/kexec/atags \
     $(DEVICE_FOLDER)/kexec/devtree:system/etc/kexec/devtree \
-    $(DEVICE_FOLDER)/kexec/kexec:system/etc/kexec/kexec \
-    $(DEVICE_FOLDER)/kexec/kexec.ko:system/etc/kexec/kexec.ko \
-    $(DEVICE_FOLDER)/kexec/uart.ko:system/etc/kexec/uart.ko \
     out/target/product/maserati/ramdisk.img:system/etc/kexec/ramdisk.img \
     out/target/product/maserati/kernel:system/etc/kexec/kernel
-
-# Permissions files
-PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:/system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/base/data/etc/android.hardware.camera.front.xml:/system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/base/data/etc/android.hardware.camera.xml:/system/etc/permissions/android.hardware.camera.xml \
-    frameworks/base/data/etc/android.hardware.location.gps.xml:/system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:/system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/base/data/etc/android.hardware.sensor.compass.xml:/system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/base/data/etc/android.hardware.sensor.light.xml:/system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:/system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:/system/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:/system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:/system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:/system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-    frameworks/base/data/etc/android.hardware.wifi.xml:/system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/base/data/etc/android.hardware.wifi.direct.xml:/system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/base/data/etc/handheld_core_hardware.xml:/system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
 
 # Prebuilts
 PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/prebuilt/bin/battd:system/bin/battd \
     $(DEVICE_FOLDER)/prebuilt/bin/mount_ext3.sh:system/bin/mount_ext3.sh \
-    $(DEVICE_FOLDER)/prebuilt/bin/strace:system/bin/strace \
-    $(DEVICE_FOLDER)/prebuilt/etc/firmware/ducati-m3.bin:/system/etc/firmware/ducati-m3.bin \
+    $(DEVICE_FOLDER)/prebuilt/etc/firmware/ducati-m3.bin:system/etc/firmware/ducati-m3.bin \
     $(DEVICE_FOLDER)/prebuilt/etc/gps.conf:system/etc/gps.conf \
     $(DEVICE_FOLDER)/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
     $(DEVICE_FOLDER)/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
@@ -153,19 +54,9 @@ PRODUCT_COPY_FILES += \
 
 # Phone settings
 PRODUCT_COPY_FILES += \
-    device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml \
-    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
-
-# we have enough storage space to hold precise GC data
-PRODUCT_TAGS += dalvik.gc.type-precise
-
-# still need to set english for audio init
-PRODUCT_LOCALES += en_US
+    device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml
 
 # stuff specific to ti OMAP4 hardware
-$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
-$(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
-$(call inherit-product-if-exists, vendor/motorola/common/proprietary/imgtec/sgx-imgtec-bins.mk)
+$(call inherit-product, device/motorola/common/common.mk)
 $(call inherit-product-if-exists, vendor/motorola/maserati/maserati-vendor.mk)
 $(call inherit-product-if-exists, vendor/verizon/verizon-ics.mk)
-$(call inherit-product-if-exists, device/ti/proprietary-open/wl12xx/wlan/wl12xx-wlan-fw-products.mk)
