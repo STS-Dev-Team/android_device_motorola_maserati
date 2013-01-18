ffa40c1ca79851499e83c4b155dfa7dc63bea0bb
dhacker29 <davidhackerdvm@gmail.com>
2013-01-18 00:49:55 -0600

    Move built audio items to common

--- device.mk
+++ device.mk
@@ -21,10 +21,6 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/audio/audio.primary.omap4.so:/system/lib/hw/audio.primary.maserati.so \
     $(DEVICE_FOLDER)/audio/libaudio_ext.so:/system/lib/libaudio_ext.so
 
-PRODUCT_PACKAGES += \
-    audio_policy.maserati \
-    libasound
-
 # Modem
 PRODUCT_PACKAGES += \
     nc \
