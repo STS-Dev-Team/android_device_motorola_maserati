3e5cfa229d00bef0d91c19f0b51e0f0fdaecf2a7
dhacker29 <davidhackerdvm@gmail.com>
2013-01-18 18:21:37 -0600

    Move dalvik heap define to device

--- device.mk
+++ device.mk
@@ -66,6 +66,7 @@ PRODUCT_COPY_FILES += $(shell \
     | tr '\n' ' ')
 endif
 
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/motorola/common/proprietary/vzw/verizon.mk)
 $(call inherit-product-if-exists, vendor/motorola/maserati/maserati-vendor.mk)
 ifneq ($(BOARD_USES_KEXEC),true)
