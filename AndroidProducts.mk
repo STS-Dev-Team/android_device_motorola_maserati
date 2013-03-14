b6d22fb6dd31ae02368b6b4807ffc690dd64ee37
dhacker29 <davidhackerdvm@gmail.com>
2013-03-14 11:40:03 -0500

    This is phablet only need cm parts

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,1 @@
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_maserati.mk
-ifeq ($(TARGET_PRODUCT),aokp_maserati)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/aokp.mk
-endif
-ifeq ($(TARGET_PRODUCT),cna_maserati)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/cna.mk
-endif
-ifeq ($(TARGET_PRODUCT),killrom_maserati)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/killrom.mk
-endif
